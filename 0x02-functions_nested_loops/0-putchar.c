7bcbc1ed002b929033cec70aa3a3ce7b0bff74aa
sammysr <samuelasres00@gmail.com>
2022-12-13 05:09:16 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+
+	_putchar('\n');
+	return (0);
+
+}
