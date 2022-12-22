2e8f1dae73f0ff9719f50d0734ed7c7a90ca0c64
sammysr <samuelasres00@gmail.com>
2022-12-22 01:49:38 -0800

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,8 +6,6 @@
  *int prototypes()
  *char prototypes()
  */
-
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -22,4 +20,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 void print_buffer(char *b, int size);
 
-#endif
+#endif /*MAIN_H*/
